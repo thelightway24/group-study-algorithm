15929e88cddc2cdb983952de0456ebfa443a6eca
chanchangood <chanwoo.kim31@gmail.com>
2025-06-15 23:16:01 +0900

    3주차(스택): 0문제 / 8문제

---
+++ Stack/1874-스택수열.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+
+	public static void solution() {
+		try (
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			bw.flush();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
