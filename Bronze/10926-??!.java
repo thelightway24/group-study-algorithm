cdb38f8703d864c5f8323045fedd1f55d782346c
chanchangood <chanwoo.kim31@gmail.com>
2025-06-29 22:42:45 +0900

    10926-??!

---
+++ Bronze/10926-??!.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+
+	public static void solution() {
+		try (
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		) {
+			String s = br.readLine();
+			bw.write(s+"??!");
+			bw.flush();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
