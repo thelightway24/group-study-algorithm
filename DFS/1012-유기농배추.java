d9a0327a47fab1395cf07080ebb4910b28f75f8b
chanchangood <chanwoo.kim31@gmail.com>
2025-07-16 22:09:57 +0900

    6주차(스택,dfs): 4문제 / 5문제

--- DFS/1012-유기농배추.java
+++ DFS/1012-유기농배추.java
@@ -13,6 +13,21 @@ public class Main {
 		) {
 			int t = Integer.parseInt(br.readLine());
 
+			for (int i = 0; i < t; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int m = Integer.parseInt(st.nextToken());
+				int n = Integer.parseInt(st.nextToken());
+				int k = Integer.parseInt(st.nextToken());
+
+				while (true){
+					StringTokenizer st1 = new StringTokenizer(br.readLine());
+					int x = Integer.parseInt(st1.nextToken());
+					int y = Integer.parseInt(st1.nextToken());
+
+				}
+
+			}
+
 			bw.flush();
 		}catch (IOException e){
 			e.printStackTrace();
