d5a3848e61f3f473bda85159a7ce7e06c33a816c
chanchangood <chanwoo.kim31@gmail.com>
2025-08-04 17:35:50 +0900

    2573-빙산

--- DFS/2573-빙산.java
+++ DFS/2573-빙산.java
@@ -30,29 +30,90 @@ public class Main {
 				}
 			}
 
-			int ans = bfs();
-			bw.write(ans + "");
+
+			int year = 0;
+			while (true) {
+				int count = countComponent();
+
+				if (count >= 2) {
+					bw.write(year + "");
+					break;
+				}
+
+				if (count == 0) {
+					bw.write("0");
+					break;
+				}
+
+				melt();
+				year++;
+			}
+
 			bw.flush();
 		}catch (IOException e){
 			e.printStackTrace();
 		}
 	}
 
-	static int bfs() {
-		int count = 0;
-		Queue<int[]> queue = new ArrayDeque<>();
+	static int countComponent() {
 		boolean[][] visited = new boolean[n][m];
+		int count = 0;
 
-		queue.add(new int[]{0, 0});
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (!visited[i][j] && map[i][j] > 0) {
+					bfs(i, j, visited);
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	static void bfs(int x, int y, boolean[][] visited) {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[]{x, y});
+		visited[x][y] = true;
 
 		while (!queue.isEmpty()) {
 			int[] cur = queue.poll();
+			int cx = cur[0];
+			int cy = cur[1];
 
+			for (int d = 0; d < 4; d++) {
+				int nx = cx + dx[d];
+				int ny = cy + dy[d];
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+				if (visited[nx][ny]) continue;
+				if (map[nx][ny] == 0) continue;
+				visited[nx][ny] = true;
+				queue.add(new int[]{nx, ny});
+			}
+		}
+	}
 
+	static void melt() {
+		int[][] minus = new int[n][m];
 
-
+		for (int x = 0; x < n; x++) {
+			for (int y = 0; y < m; y++) {
+				if (map[x][y] > 0) {
+					int sea = 0;
+					for (int d = 0; d < 4; d++) {
+						int nx = x + dx[d];
+						int ny = y + dy[d];
+						if (map[nx][ny] == 0) sea++;
+					}
+					minus[x][y] = sea;
+				}
+			}
 		}
 
-		return count;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				map[i][j] -= minus[i][j];
+				if (map[i][j] < 0) map[i][j] = 0;
+			}
+		}
 	}
 }
